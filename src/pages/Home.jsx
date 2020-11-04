e93ec1cd12462b5647688da4fbd585b6747d2dc1
tuxingxiang <tuxingxiang@gentek.com.cn>
2020-11-04 13:58:08 +0800

    project init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Start</Button>
+      </div>
+    );
+  }
+}
+
+export default Home;
