03c75ea38b8333ffd7adabe4110e12e8ea013b7f
tuxingxiang <tuxingxiang@gentek.com.cn>
2020-11-05 11:27:37 +0800

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../style/login.scss";
-import { InputItem, Button } from "antd-mobile";
+import { InputItem, Button, WingBlank, WhiteSpace } from "antd-mobile";
 import { createForm } from "rc-form";
 
 class Login extends Component {
@@ -18,21 +18,28 @@ class Login extends Component {
     const { getFieldProps } = this.props.form;
     return (
       <div className="login">
-        <InputItem
-          {...getFieldProps("username")}
-          clear
-          placeholder="请输入账号"
-        >
-          账号：
-        </InputItem>
-        <InputItem
-          {...getFieldProps("password")}
-          type="password"
-          placeholder="请输入密码"
-        >
-          密码：
-        </InputItem>
-        <Button onClick={this.onSubmit}>提交</Button>
+        <WingBlank>
+          <InputItem
+            {...getFieldProps("username")}
+            labelNumber="3"
+            placeholder="请输入账号"
+          >
+            账号：
+          </InputItem>
+          <WhiteSpace />
+        </WingBlank>
+        <WingBlank>
+          <InputItem
+            {...getFieldProps("password")}
+            labelNumber="3"
+            type="password"
+            placeholder="请输入密码"
+          >
+            密码：
+          </InputItem>
+          <WhiteSpace />
+        </WingBlank>
+        <Button className="login-submit" onClick={this.onSubmit}>提交</Button>
       </div>
     );
   }
